96067746aed9fd8ca7aa0b0d3efcd91ed577fc44
tomutanyi <tomutanyi@gmail.com>
2022-10-14 09:08:14 -0700

    correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
 * main - Prints out sentence
 *
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
-	return (1);
+write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+return (1);
 }
