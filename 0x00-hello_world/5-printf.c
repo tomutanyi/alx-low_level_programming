066bfec15ed219010a0d62e453fdc3aaf22ab38e
tomutanyi <tomutanyi@gmail.com>
2022-10-13 23:09:16 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main - Main function
+*
+*
+* Return: return 0 every time
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome isa piece of art,\n");
+	return (0);
+}
