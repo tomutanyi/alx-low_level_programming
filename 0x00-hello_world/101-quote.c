e35c1a791151bea2ed508b915b10453e3c15af5b
tomutanyi <tomutanyi@gmail.com>
2022-10-14 09:04:32 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 /**
-* main - Entry point
+* main - Prints out sentence
 *
 *
-* Return: Always 1 (Success)
+* Return: 1 if success
 */
 
 int main(void)
 {
-	char ch = "and that piece of art is useful\" - Dora Kopar, 2015-10-19";
-
-	putchar(ch);
-
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
