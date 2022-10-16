76d194a30cb7813382a9f9879cd56381dc636069
tomutanyi <tomutanyi@gmail.com>
2022-10-15 17:02:06 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int rim;
+
+	for (rim = 0; rim < 10; rim++)
+		printf("%d", rim);
+	putchar('\n');
+	return (0);
+}
