04fd420f34c51aafeeb900aaad16a1e83c832c5f
tomutanyi <tomutanyi@gmail.com>
2022-10-21 04:30:27 -0700

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,12 +8,14 @@
  */
 void print_line(int n)
 {
-	int i = 0;
+	int i;
 
 	if (n > 0)
 	{
-		for (i <= n; i++)
-			_putchar('_');
+		for (i = 0; i < n; i++)
+		{
+			_putchar (95);
+		}
 	}
 	_putchar('\n');
 
