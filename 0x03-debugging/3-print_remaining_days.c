565a20f6946b19a6e3d239df8ef053f5ce5407eb
tomutanyi <tomutanyi@gmail.com>
2022-10-21 01:07:54 -0700

    Correction

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,7 +14,7 @@ void print_remaining_days(int day, int month, int year)
 {
 	if (year % 4 == 0 || ((year % 100 == 0) && (year % 400 == 0)))
 	{
-		if (month > 2 && day >= 60)
+		if (month > 3 && day >= 60)
 		{
 			day++;
 		}
