2926134e93c9491500620182e5cd309ab36da257
tomutanyi <tomutanyi@gmail.com>
2022-10-21 03:36:03 -0700

    correction of indentation

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -4,16 +4,17 @@
  * @c: the digit to be checked.
  * Return: (1) if c is a digit, 0 if otherwise.
  */
+
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
-	{
-		return (1);
-	}
+if (c >= 0 && c <= 9)
+{
+return (1);
+}
 
-	else
-	{
-		return (1);
-	}
+else
+{
+return (0);
+}
 
 }
