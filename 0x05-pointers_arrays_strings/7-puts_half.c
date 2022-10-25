6aa007c3c536a1a3680b5be7b1f19b088777bd4d
tomutanyi <tomutanyi@gmail.com>
2022-10-25 06:45:26 -0700

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints only HALF of a string to be displayed.
+ * @str: The string to be printed and returned.
+ * Return: ALWAYS (0).
+ */
+
+void puts_half(char *str)
+{
+	int liv = 0, day = 0, z;
+
+	while (str[liv++])
+		day++;
+
+	if ((day % 2) == 0)
+		z = day / 2;
+
+	else
+		z = (day + 1) / 2;
+
+	for (liv = z; liv < day; liv++)
+		_putchar(str[liv]);
+
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,6 +3,7 @@
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+void puts_half(char *str);
 int _strlen(char *s);
 int _putchar(char c);
 void _puts(char *str);
