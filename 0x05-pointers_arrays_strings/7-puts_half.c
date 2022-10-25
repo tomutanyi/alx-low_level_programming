10ec097a9162db18b1118a6ce8d6ae4cc7179c99
tomutanyi <tomutanyi@gmail.com>
2022-10-25 06:47:20 -0700

    correction of parentheses

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,16 +11,24 @@ void puts_half(char *str)
 	int liv = 0, day = 0, z;
 
 	while (str[liv++])
+	{
 		day++;
+	}
 
 	if ((day % 2) == 0)
+	{
 		z = day / 2;
+	}
 
 	else
+	{
 		z = (day + 1) / 2;
+	}
 
 	for (liv = z; liv < day; liv++)
+	{
 		_putchar(str[liv]);
+	}
 
 	_putchar('\n');
 }
