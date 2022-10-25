013d6c35c1f7da2e795c1353350850f06aa2530c
tomutanyi <tomutanyi@gmail.com>
2022-10-25 06:35:04 -0700

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts2 - A function that prints all even chars of a string.
+ * @str: The string whose even numbered chars are displayed.
+ * Return: Always (0).
+ */
+void puts2(char *str)
+{
+	int o;
+	char k;
+
+	for (o = 0; str[0] != 0; o++)
+	{
+		if (o % 2 == 0)
+		{
+			k = str[o];
+			_putchar(k);
+		}
+	}
+	_putchar('\n');
+}
