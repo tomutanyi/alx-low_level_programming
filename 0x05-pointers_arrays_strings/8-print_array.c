86e1ce88d2fab85b312799b83c34deeb27ea080f
tomutanyi <94693960+tomutanyi@users.noreply.github.com>
2022-11-01 01:11:58 +0300

    correction

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,21 +1,22 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_array - Prints the inputted n0 of elements
- * of an array of integers.
- * @a: The array of int to be displayed
- * @n: The number of elements to be printed.
+ * print_array - Prints the inputted n0 of elements from usr
+ * of an array of integers from usr
+ * @a: The array of int to be displayed from usr
+ * @n: The number of elements to be printed by function proottype
  */
+
 void print_array(int *a, int n)
 {
-	int zit;
+	int ziti;
 
-	for (zit = 0; zit < n; zit++)
+	for (ziti = 0; ziti < n; ziti++)
 	{
-		printf("%d", a[zit]);
+		printf("%d", a[ziti]);
 
-		if (zit == n - 1)
+		if (ziti == n - 1)
 		{
 			continue;
 		}
