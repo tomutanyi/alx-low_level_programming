ee27b3ebd10ea81072ed596acce53ad32a476383
tomutanyi <tomutanyi@gmail.com>
2022-11-01 07:27:15 -0700

    task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _memcpy - copies memory area
+ * @src: the original string
+ * @dest: the destination str .
+ * @n: the no. of bytes to be copied
+ * Return: pointer to the destination .
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *sol = dest;
+
+	while (n--)
+	{
+		*dest++ = *src++;
+	}
+
+	return (sol);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,2 +1,3 @@
 void set_string(char **s, char *to);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
