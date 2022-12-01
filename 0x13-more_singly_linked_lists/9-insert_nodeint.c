f7bfd3376171367e8c75f1e96e529e38ac45953d
tomutanyi <tomutanyi@gmail.com>
2022-12-01 10:22:04 -0800

    task 9 correction 2

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -6,58 +6,34 @@
  * @idx: index of node .
  * @n: content of node .
  * Return: address of node
- */
+ **/
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *pri_node = NULL;
-	listint_t *old_node = NULL;
-	unsigned int k = 0;
+	listint_t *node, *temp;
+	unsigned int l = 0;
 
-	new_node = malloc(sizeof(listint_t));
-	if (pri_node == NULL || idx > listint_len(*head))
+	if (*head == NULL && idx != 0)
+		return (NULL);
+	if (idx != 0)
 	{
-		free(pri_node);
+	temp = *head;
+	for (; l < idx - 1 && temp != NULL; l++)
+		temp = temp->next;
+	if (temp == NULL)
 		return (NULL);
 	}
-	pri_node->n = n;
-	pri_node->next = NULL;
-	while (head != NULL)
+	node = malloc(sizeof(listint_t));
+	if (node == NULL)
+		return (NULL);
+	node->n = n;
+	if (idx == 0)
 	{
-		if (k == idx)
-		{
-			if (k == 0)
-			{
-				pri_node->next = *head;
-				*head = pri_node;
-				return (pri_node);
-			}
-			pri_node->next = old_node->next;
-			old_node->next = pri_node;
-			return (pri_node);
-		}
-		else if ((k + 1) == idx)
-			old_node = *head;
-		head = &((*head)->next);
-		k++;
+		node->next = *head;
+		*head = node;
+		return (node);
 	}
-	return (NULL);
-}
-
-/**
- * listint_len - counts nodes in list .
- * @h: head of list .
- * Return: no. elements .
- */
-size_t listint_len(const listint_t *h)
-{
-	const listint_t *mouse = h;
-	size_t hsb = 0;
-
-	while (mouse != NULL)
-	{
-		hsb += 1;
-		mouse = mouse->next;
-	}
-	return (hsb);
+	node->next = temp->next;
+	temp->next = node;
+	return (node);
 }
