9f772cae2101a21af93d8cb2f6b92933b64ec740
tomutanyi <tomutanyi@gmail.com>
2022-12-01 10:17:07 -0800

    task 9 correction

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,39 +1,63 @@
 #include "lists.h"
+
 /**
- * insert_nodeint_at_index - insert node .
- * @head: 1st node address .
- * @idx: position of node .
- * @n: data of node .
- * Return: address of node .
- **/
+ * insert_nodeint_at_index - returns node of list .
+ * @head: ptr to head .
+ * @idx: index of node .
+ * @n: content of node .
+ * Return: address of node
+ */
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *shan, *temp;
-	unsigned int p = 0;
+	listint_t *pri_node = NULL;
+	listint_t *old_node = NULL;
+	unsigned int k = 0;
 
-	if (*head == NULL && idx != 0)
-		return (NULL);
-	if (idx != 0)
+	new_node = malloc(sizeof(listint_t));
+	if (pri_node == NULL || idx > listint_len(*head))
 	{
-		temp = *head;
-		for (; p < idx - 1 && temp != NULL; p++)
-		{
-			temp = temp->next;
-		}
-		if (temp == NULL)
-		{
-			return (NULL);
-		}
-	}
-	shan = malloc(sizeof(listint_t));
-	if (shan == NULL)
+		free(pri_node);
 		return (NULL);
-	shan->n = n;
-	if (idx == 0)
-		shan->next = *head;
-		*head = shan;
-		return (shan);
-	shan->next = temp->next;
-	temp->next = shan;
-	return (shan);
+	}
+	pri_node->n = n;
+	pri_node->next = NULL;
+	while (head != NULL)
+	{
+		if (k == idx)
+		{
+			if (k == 0)
+			{
+				pri_node->next = *head;
+				*head = pri_node;
+				return (pri_node);
+			}
+			pri_node->next = old_node->next;
+			old_node->next = pri_node;
+			return (pri_node);
+		}
+		else if ((k + 1) == idx)
+			old_node = *head;
+		head = &((*head)->next);
+		k++;
+	}
+	return (NULL);
+}
+
+/**
+ * listint_len - counts nodes in list .
+ * @h: head of list .
+ * Return: no. elements .
+ */
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *mouse = h;
+	size_t hsb = 0;
+
+	while (mouse != NULL)
+	{
+		hsb += 1;
+		mouse = mouse->next;
+	}
+	return (hsb);
 }
