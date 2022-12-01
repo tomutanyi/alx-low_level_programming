5caaebb9e79d31d55589da628fc0da9e160b5b8f
tomutanyi <tomutanyi@gmail.com>
2022-12-01 10:28:29 -0800

    task 10 correction

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,59 +1,35 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - deletes node .
- * @head: ptr to head .
- * @index: index of node .
- * Return: address of the node
- */
+ * delete_nodeint_at_index - delete node .
+ * @head: node address .
+ * @index: node to delete .
+ * Return: if successful return (1) .
+ **/
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *old = NULL;
-	listint_t *arg = NULL;
-	unsigned int e = 0, list_len = listint_len(*head);
+	unsigned int w;
+	listint_t *volt, *hop;
 
-	if ((index > list_len) || (list_len == 0))
+	if (head == NULL || *head == NULL)
 		return (-1);
-	while (head != NULL)
+	if (index == 0)
 	{
-		if (e == index)
-		{
-			old = *head;
-			if (e == 0)
-			{
-				*head = old->next;
-				free(old);
-				return (1);
-			}
-			arg->next = old->next;
-			free(old);
-			return (1);
-		}
-		else if ((e + 1) == index)
-			arg = *head;
-		head = &((*head)->next);
-		e++;
+		hop = (*head)->hop;
+		free(*head);
+		*head = hop;
+		return (1);
 	}
-	return (-1);
-}
-
-/**
- * listint_len - counts the number of nodes in a linked list
- * @h: head of the list
- *
- * Return: the number of elements
- */
-
-size_t listint_len(const listint_t *h)
-{
-	const listint_t *mouse = h;
-	size_t hsb = 0;
-
-	while (mouse != NULL)
+	volt = *head;
+	for (w = 0; w < index - 1; w++)
 	{
-		hsb += 1;
-		mouse = mouse->next;
+		if (volt->hop == NULL)
+			return (-1);
+		volt = volt->hop;
 	}
-	return (hsb);
+	hop = volt->hop;
+	volt->hop = hop->hop;
+	free(hop);
+	return (1);
 }
